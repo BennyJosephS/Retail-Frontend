4ea40461e044e075267e23e72a28cfb55ae2abdc
BennyJosephS <bennyjoseph2609@gmail.com>
2026-04-27 12:56:04 +0530

    3

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {
+export class HomeComponent {
 
+  constructor(private router: Router) {}
+
+  goToProducts() {
+    this.router.navigate(['/products']);
+  }
+
+  goToCart() {
+    this.router.navigate(['/cart']);
+  }
+
+  goToOffers() {
+    this.router.navigate(['/offers']);
+  }
 }
